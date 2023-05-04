c65bf1eef0cc674e99d0686f181d6b336bd1d321
siyamtanda <68695019+siyamtanda@users.noreply.github.com>
2023-05-04 15:39:25 +0200

    Update admin.py
    This file  inherits from django.contrib.admin.ModelAdmin and overrides the save_model method to log user login/logout activity.

--- profiles_tests/admin.py
+++ profiles_tests/admin.py
@@ -1,3 +1,24 @@
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'home_address', 'phone_number', 'location')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('home_address', 'phone_number', 'location')}),
+    )
+
+admin.site.unregister(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
+
+class CustomAdminSite(admin.AdminSite):
+    def has_permission(self, request):
+        return request.user.is_active and request.user.is_superuser
+
+custom_admin_site = CustomAdminSite(name='myadmin')
+custom_admin_site.register(CustomUser, CustomUserAdmin)
+
 
 # Register your models here.
