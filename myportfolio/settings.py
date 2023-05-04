87c30ccc836e1635eba2d2792d8812a50dd2f5ff
siyamtanda <68695019+siyamtanda@users.noreply.github.com>
2023-05-04 15:51:19 +0200

    Updating setting.py
    specifying the model to use for user authentication and to enable logging of user login/logout activity

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -104,6 +105,24 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'auth.User'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.contrib.auth': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
 
 
 # Internationalization
